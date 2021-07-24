9c8feaa661897955dd72cb56f638ec23b7283f8a
sietzemm@hotmail.com <sietzemm@hotmail.com>
2021-07-24 14:51:32 +0200

    added button functionality to pacmin

--- PacMin/PacMin.py
+++ PacMin/PacMin.py
@@ -1,5 +1,5 @@
 # Author : Sietze Min
-# Date : 11-06-2020, last modified on : 12-06-2020
+# Date : 11-06-2020, last modified on : 23-07-2021
 # Description : Recreation of the classic Pac man arcade game from scratch
 
 import sys
@@ -11,40 +11,55 @@ from pynput import keyboard
 class Game:
     def __init__(self, root):
         self.root = root
-        self.title = "PacMin"
         self.screenHeight = root.winfo_screenheight()
         self.screenWidth = root.winfo_screenwidth()
-        self.screen = Canvas(root, width = self.screenWidth, height = self.screenHeight, bg="black")
-        self.screen.pack()
-        self.screen.create_text(self.screenWidth/2,self.screenHeight/2,fill="white",font="Times 40 bold",text="PACMIN 2020")
-
-        self.startScreenBool = True
-        self.gameScreenBool = False
-        self.scoreScreen = False
-
-        self.foodLocation = []
-        
-        if self.startScreenBool == True:
-            self.startScreen(self.screen, self.root)
-
-    def startGame(self):
-        pass
-
-    def startScreen(self, screen, root):
-        screen.create_text(self.screenWidth/2, ((self.screenHeight/2)+50), fill = "white", font="Times 20", text = "Made by Sietze Min")
-        startBTN = screen.create_rectangle(self.screenWidth/2, self.screenWidth/2, (self.screenWidth/2)+100, (self.screenWidth/2)+20, fill="green", outline="grey60")
-        screen.create_text(self.screenWidth/2, ((self.screenHeight/2)+100), fill= "white", text="Start new game")
+        # self.screen = Canvas(root, width = self.screenWidth, height = self.screenHeight, bg="black")
     
-        # StartScreen(self.screen, self.root)
-        
-    def updateGameScreen(self, root):
-        pass
+        self.startScreenBool = True
+        # self.gameScreenBool = False
+        # self.scoreScreen = False
 
-    def placeFood(self, root):
-        pass
+        def startNewGameCallBack():
+            print("execute newGameCallBack")
+            self.startScreenBool = False
+            self.root.after(100, main)
 
-    def placePowerUp(self, root):
-        pass
+        def main():
+  
+            # init start screen
+            if(self.startScreenBool == True):
+
+                self.frameHeader = Frame(self.root, width = self.screenWidth, height=300, bg="black")
+                self.frameHeader.grid(row=1, column=1)
+
+                self.txtLbl01 = Label(self.root, text="PacMin", fg="white", bg="black", font=("Arial",25))
+                self.txtLbl01.grid(row=2, column=1)
+
+                self.txtLbl02 = Label(self.root, text="HIGHSCORES", bg="black", fg="white")
+                self.txtLbl02.grid(row=3, column=1)
+
+                self.newGameBtn = Button(self.root, text="new game", bg="gray", command = startNewGameCallBack, cursor="hand1")
+                self.newGameBtn.grid(row=4, column=1)
+
+                self.footerTxtLbl = Label(self.root, text="Made by Sietze Min", bg="black", fg="white")
+                self.footerTxtLbl.grid(row=5, column=1, pady=(100,0))
+
+                self.frameFooter = Frame(self.root, width=self.screenWidth, height=50, bg="black" )
+                self.frameFooter.grid(row=6, column=1, sticky=S)
+            else:
+                self.txtLbl01.destroy()
+                self.newGameBtn.destroy()
+                self.txtLbl02.destroy()
+                print("Else clause activated")
+
+        # Initial execution
+        main()
+
+    # def placeFood(self, root):
+    #     pass
+
+    # def placePowerUp(self, root):
+    #     pass
 
 
 class Ghost:
@@ -84,5 +99,6 @@ class PowerUp:
 
 
 root = Tk()
+root.title = "PacMin"
 app = Game(root)
 root.mainloop()
